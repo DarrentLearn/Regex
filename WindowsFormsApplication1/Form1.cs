801475272a3ea2b5c43b5de70edf6ad59e427027
Darrent Chen <44484851+DarrentLearn@users.noreply.github.com>
2019-01-10 19:57:20 +0800

    Add files via upload

---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            string[] values =
+            {
+                "可，一XXX15.5核發",
+                "可，萬一XXX15.5a核發",
+                "可，一XXX15.5a萬核發",
+                "可，萬一XXX15.5a萬核發",
+                "可，一XXX15.5萬核發",
+                "可，萬一XXX15.5萬核發",
+                "可，萬一XXX15萬核發",
+                "可，萬一XXX15.3萬或34萬核發",
+            };
+            Regex rx = new Regex(@"\d萬");
+
+            foreach (string value in values)
+            {
+                var rs = rx.IsMatch(value);
+                if (rs)
+                {
+                    MatchCollection matches = rx.Matches(value); 
+                    Console.Write("{0} matches found in:\t   {1}\t", matches.Count, value); 
+                    foreach (Match match in matches)
+                    {
+                        GroupCollection groups = match.Groups;
+                        Console.Write(", at positions {0}",  groups[0].Index);
+                    }
+                    Console.WriteLine("");
+                }
+            }
+        }
+    }
+}
